b53316f0953cedd60dd2aab02f73e259e3c6eb54
Rillary Sousa <rillarysousa28@gmail.com>
2025-10-20 11:14:01 -0300

    UPDATE - Feature 2: Botão limpar adicionado e funcional no JavaScript

--- script.js
+++ script.js
@@ -12,10 +12,19 @@ window.document.addEventListener("DOMContentLoaded", function(){
         document.querySelector("#result").innerHTML = inputText.toLowerCase()
     })
 
-    // Botão: Letra Minúscula
+    // Botão: Primeira letra Maiúscula
     window.document.querySelector("#btnPrimeira").addEventListener("click", function(){
         let inputText = document.querySelector("#input-text").value.toLowerCase()
         document.querySelector("#result").innerHTML = inputText.charAt(0).toUpperCase() + inputText.slice(1);
     })
+
+    // Botão: Limpar
+    window.document.querySelector("#btnLimpar").addEventListener("click", function(){
+        document.querySelector("#input-text").value = ""; 
+        document.querySelector("#result").innerHTML = "";
+});
+
+
+
 
 }); 
